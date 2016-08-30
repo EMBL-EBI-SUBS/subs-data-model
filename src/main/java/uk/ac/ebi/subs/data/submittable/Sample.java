f640be597f6f54965199b64c5ccfb5c6ee2dc912
fpenim <flaviapenim@gmail.com>
2016-08-30 16:08:29 +0100

    Added sample mongo DB repository

--- src/main/java/uk/ac/ebi/subs/data/submittable/Sample.java
+++ src/main/java/uk/ac/ebi/subs/data/submittable/Sample.java
@@ -1,5 +1,6 @@
 package uk.ac.ebi.subs.data.submittable;
 
+import org.springframework.data.annotation.Id;
 import uk.ac.ebi.subs.data.AbstractSubsEntity;
 import uk.ac.ebi.subs.data.component.SampleLink;
 
@@ -7,9 +8,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Sample extends AbstractSubsEntity {
+
+    @Id
+    String id;
+
     List<SampleLink> sampleLinks = new ArrayList<>();
     Integer taxonId;
     String taxon;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public List<SampleLink> getSampleLinks() {
         return sampleLinks;
