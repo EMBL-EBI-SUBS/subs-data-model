48555f407ab1080eded74b4f472bb6e0bdc73800
David Richardson <davidr@ebi.ac.uk>
2016-09-05 09:53:02 +0100

    Updated UML diagrams

--- src/main/java/uk/ac/ebi/subs/data/submittable/Analysis.java
+++ src/main/java/uk/ac/ebi/subs/data/submittable/Analysis.java
@@ -8,23 +8,23 @@ import java.util.List;
 
 public class Analysis extends AbstractSubsEntity<Analysis> implements Files {
 
-    List<AbstractSubsEntity> embeddedEntities  =  new ArrayList<AbstractSubsEntity>();
-    List<SubsLink> references = new ArrayList<SubsLink>();
+    List<Analysis> embeddedEntities  =  new ArrayList<>();
+    List<SubsLink<Submittable>> references = new ArrayList<SubsLink<Submittable>>();
     List<File> files = new ArrayList<File>();
 
-    public List<AbstractSubsEntity> getEmbeddedEntities() {
+    public List<Analysis> getEmbeddedEntities() {
         return embeddedEntities;
     }
 
-    public void setEmbeddedEntities(List<AbstractSubsEntity> embeddedEntities) {
+    public void setEmbeddedEntities(List<Analysis> embeddedEntities) {
         this.embeddedEntities = embeddedEntities;
     }
 
-    public List<SubsLink> getReferences() {
+    public List<SubsLink<Submittable>> getReferences() {
         return references;
     }
 
-    public void setReferences(List<SubsLink> references) {
+    public void setReferences(List<SubsLink<Submittable>> references) {
         this.references = references;
     }
 
