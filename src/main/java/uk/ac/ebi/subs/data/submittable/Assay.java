beaca14ce196ccdcd6a618974e5e32f9929f0e6b
David Richardson <davidr@ebi.ac.uk>
2016-10-11 11:46:40 +0100

    use multiple samples and protocols in assay

--- src/main/java/uk/ac/ebi/subs/data/submittable/Assay.java
+++ src/main/java/uk/ac/ebi/subs/data/submittable/Assay.java
@@ -1,14 +1,17 @@
 package uk.ac.ebi.subs.data.submittable;
 
-import uk.ac.ebi.subs.data.component.AbstractSubsRef;
-import uk.ac.ebi.subs.data.component.AssayRef;
-import uk.ac.ebi.subs.data.component.SampleRef;
-import uk.ac.ebi.subs.data.component.StudyRef;
+import uk.ac.ebi.subs.data.component.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class Assay extends AbstractSubsEntity<Assay> {
 
     StudyRef studyRef = new StudyRef();
-    SampleRef sampleRef = new SampleRef();
+    List<SampleUse> sampleUses = new ArrayList<>();
+    List<ProtocolUse> protocolUses = new ArrayList<>();
+
 
     public StudyRef getStudyRef() {
         return studyRef;
@@ -18,16 +21,49 @@ public class Assay extends AbstractSubsEntity<Assay> {
         this.studyRef = studyRef;
     }
 
-    public SampleRef getSampleRef() {
-        return sampleRef;
+    public List<SampleUse> getSampleUses() {
+        return sampleUses;
     }
 
-    public void setSampleRef(SampleRef sampleRef) {
-        this.sampleRef = sampleRef;
+    public void setSampleUses(List<SampleUse> sampleUses) {
+        this.sampleUses = sampleUses;
+    }
+
+    public List<ProtocolUse> getProtocolUses() {
+        return protocolUses;
+    }
+
+    public void setProtocolUses(List<ProtocolUse> protocolUses) {
+        this.protocolUses = protocolUses;
     }
 
     @Override
     protected AssayRef newRef() {
         return new AssayRef();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Assay assay = (Assay) o;
+        return Objects.equals(studyRef, assay.studyRef) &&
+                Objects.equals(sampleUses, assay.sampleUses) &&
+                Objects.equals(protocolUses, assay.protocolUses);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), studyRef, sampleUses, protocolUses);
+    }
+
+    @Override
+    public String toString() {
+        return "Assay{" +
+                "studyRef=" + studyRef +
+                ", sampleUses=" + sampleUses +
+                ", protocolUses=" + protocolUses +
+                "} " + super.toString();
     }
 }
