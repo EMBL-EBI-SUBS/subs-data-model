984169e9966399fa7d69f5f99a6e1543775069a3
David Richardson <davidr@ebi.ac.uk>
2016-12-20 16:07:43 +0000

    remove referenced object from AbstractSubsRef

--- src/main/java/uk/ac/ebi/subs/data/component/AssayRef.java
+++ src/main/java/uk/ac/ebi/subs/data/component/AssayRef.java
@@ -3,15 +3,5 @@ package uk.ac.ebi.subs.data.component;
 import uk.ac.ebi.subs.data.submittable.Assay;
 
 public class AssayRef extends AbstractSubsRef<Assay> {
-    Assay referencedObject;
 
-    @Override
-    public Assay getReferencedObject() {
-        return referencedObject;
-    }
-
-    @Override
-    public void setReferencedObject(Assay referencedObject) {
-        this.referencedObject = referencedObject;
-    }
 }
