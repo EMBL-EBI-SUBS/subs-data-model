bbf2de5f82ec2f5a6ace756ad61a6905cac828fc
David Richardson <davidr@ebi.ac.uk>
2017-01-05 10:47:59 +0000

    move processing status to data model package and update status list

---
+++ src/main/java/uk/ac/ebi/subs/data/status/ProcessingStatus.java
@@ -0,0 +1,14 @@
+package uk.ac.ebi.subs.data.status;
+
+public enum ProcessingStatus {
+    Draft, /* exists in the usi system */
+    Submitted, /* user has Submitted document */
+    Dispatched, /* usi has dispatched object to archive */
+    Received, /* archive has received object */
+    Curation, /* archive is curating object */
+    ActionRequired, /* curator has requested changes from user*/
+    Accepted, /* archive has accepted object */
+    Processing, /* archive is processing object */
+    Done /* archive has processed object */
+    ;
+}
