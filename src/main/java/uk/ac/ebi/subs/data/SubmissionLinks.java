6f56f32ec94030773a54f5192fff98bad892b3f8
David Richardson <davidr@ebi.ac.uk>
2017-02-02 16:36:50 +0000

    use better rel names

--- src/main/java/uk/ac/ebi/subs/data/SubmissionLinks.java
+++ src/main/java/uk/ac/ebi/subs/data/SubmissionLinks.java
@@ -2,16 +2,16 @@ package uk.ac.ebi.subs.data;
 
 public class SubmissionLinks {
 
-    public static final String ANALYSIS = "submissionAnalyses";
-    public static final String ASSAY_DATA = "submissionAssayData";
-    public static final String ASSAY = "submissionAssays";
-    public static final String EGA_DAC_POLICY = "submissionEgaDacPolicies";
-    public static final String EGA_DAC = "submissionEgaDacs";
-    public static final String EGA_DATASET = "submissionEgaDatasets";
-    public static final String PROJECT = "submissionProjects";
-    public static final String PROTOCOL = "submissionProtocols";
-    public static final String SAMPLE_GROUP = "submissionSampleGroups";
-    public static final String SAMPLE = "submissionSamples";
-    public static final String STUDY = "submissionStudies";
+    public static final String ANALYSIS = "analyses";
+    public static final String ASSAY_DATA = "assayData";
+    public static final String ASSAY = "assays";
+    public static final String EGA_DAC_POLICY = "egaDacPolicies";
+    public static final String EGA_DAC = "egaDacs";
+    public static final String EGA_DATASET = "egaDatasets";
+    public static final String PROJECT = "projects";
+    public static final String PROTOCOL = "protocols";
+    public static final String SAMPLE_GROUP = "sampleGroups";
+    public static final String SAMPLE = "samples";
+    public static final String STUDY = "studies";
 
 }
