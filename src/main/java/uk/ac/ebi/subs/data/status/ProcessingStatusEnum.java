ecaebbf3b307fb954387ea23eab64b9813c98b4c
Neil Goodgame <neilg@ebi.ac.uk>
2017-03-02 12:42:48 +0000

    Added error status

--- src/main/java/uk/ac/ebi/subs/data/status/ProcessingStatusEnum.java
+++ src/main/java/uk/ac/ebi/subs/data/status/ProcessingStatusEnum.java
@@ -9,6 +9,7 @@ public enum ProcessingStatusEnum {
     ActionRequired, /* curator has requested changes from user*/
     Accepted, /* archive has accepted object */
     Processing, /* archive is processing object */
-    Done /* archive has processed object */
+    Done, /* archive has processed object */
+    Error /* archive has rejected the submitted document */
     ;
 }
