461aca9ad6a680450aa98d50581c97e8a6e3251b
Flavia Penim <flaviapenim@gmail.com>
2017-04-26 11:21:34 +0100

    SampleRelationShip fixed.

--- src/main/java/uk/ac/ebi/subs/data/component/SampleRelationship.java
+++ src/main/java/uk/ac/ebi/subs/data/component/SampleRelationship.java
@@ -3,13 +3,11 @@ package uk.ac.ebi.subs.data.component;
 
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
-import uk.ac.ebi.subs.data.submittable.Sample;
 
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class SampleRelationship extends SampleRef {
     private String relationshipNature; // e.g. Child of
-    private String targetAccession;
 
     public String getRelationshipNature() {
         return relationshipNature;
@@ -19,11 +17,4 @@ public class SampleRelationship extends SampleRef {
         this.relationshipNature = relationshipNature;
     }
 
-    public String getTargetAccession() {
-        return targetAccession;
-    }
-
-    public void setTargetAccession(String targetAccession) {
-        this.targetAccession = targetAccession;
-    }
 }
