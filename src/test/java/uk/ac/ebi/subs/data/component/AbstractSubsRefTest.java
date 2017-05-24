1f250e2412a7f596b0991063bd2bf3eaf19c3b10
Rolando Fernandez <rolando@ebi.ac.uk>
2017-05-24 17:07:16 +0100

    isAccessioned() tests

--- src/test/java/uk/ac/ebi/subs/data/component/AbstractSubsRefTest.java
+++ src/test/java/uk/ac/ebi/subs/data/component/AbstractSubsRefTest.java
@@ -1,7 +1,28 @@
 package uk.ac.ebi.subs.data.component;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by rolando on 22/05/2017.
  */
 public class AbstractSubsRefTest {
+
+    private class MockRefClass extends AbstractSubsRef { }
+
+    @Test
+    public void testIsAccessioned_HasAccession() {
+        MockRefClass mockRefObject = new MockRefClass();
+        mockRefObject.setAccession("test");
+
+        assertTrue(mockRefObject.isAccessioned());
+    }
+
+    @Test
+    public void testIsAccessioned_HasNoAccession() {
+        MockRefClass mockRefObject = new MockRefClass();
+
+        assertFalse(mockRefObject.isAccessioned());
+    }
 }
