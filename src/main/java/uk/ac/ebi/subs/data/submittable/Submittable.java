73ab7a2c5a51196ea04c59fee94fefa9bd14e1ba
David Richardson <davidr@ebi.ac.uk>
2018-02-06 14:58:55 +0000

    breaking changes to the model
    Convert assay data to have a list of assay refs, remove the sample ref.
    Analysis needs protocol uses

--- src/main/java/uk/ac/ebi/subs/data/submittable/Submittable.java
+++ src/main/java/uk/ac/ebi/subs/data/submittable/Submittable.java
@@ -1,10 +1,12 @@
 package uk.ac.ebi.subs.data.submittable;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import uk.ac.ebi.subs.data.component.AbstractSubsRef;
 import uk.ac.ebi.subs.data.component.Attribute;
 import uk.ac.ebi.subs.data.component.Attributes;
 import uk.ac.ebi.subs.data.component.Team;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface Submittable extends Attributes{
