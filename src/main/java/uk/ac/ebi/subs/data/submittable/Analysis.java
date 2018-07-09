bda19e0e43f6110b996fedc479d000f409f78516
David Richardson <davidr@ebi.ac.uk>
2018-07-09 16:26:57 +0100

    add analysisType to analysis

--- src/main/java/uk/ac/ebi/subs/data/submittable/Analysis.java
+++ src/main/java/uk/ac/ebi/subs/data/submittable/Analysis.java
@@ -15,6 +15,7 @@ import java.util.stream.Stream;
 @EqualsAndHashCode(callSuper = true)
 public class Analysis extends BaseSubmittable<Analysis> implements Files {
 
+    private String analysisType;
     private List<AnalysisRef> analysisRefs = new ArrayList<>();
     private List<AssayDataRef> assayDataRefs = new ArrayList<>();
     private List<StudyRef> studyRefs = new ArrayList<>();
@@ -22,7 +23,6 @@ public class Analysis extends BaseSubmittable<Analysis> implements Files {
     private List<AssayRef> assayRefs = new ArrayList<>();
 
     private List<ProtocolUse> protocolUses = new ArrayList<>();
-
     private List<File> files = new ArrayList<File>();
 
     @Override
@@ -88,5 +88,11 @@ public class Analysis extends BaseSubmittable<Analysis> implements Files {
         this.protocolUses = protocolUses;
     }
 
+    public String getAnalysisType() {
+        return analysisType;
+    }
 
+    public void setAnalysisType(String analysisType) {
+        this.analysisType = analysisType;
+    }
 }
