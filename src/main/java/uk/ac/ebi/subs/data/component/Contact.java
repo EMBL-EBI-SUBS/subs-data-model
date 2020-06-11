0ccf7c2cc0a96f8a10b71405225121cae6a7c8fc
Karoly Erdos <karoly@ebi.ac.uk>
2020-06-11 16:45:53 +0100

    Add fields for affiliation's URI and role to Contacts

--- src/main/java/uk/ac/ebi/subs/data/component/Contact.java
+++ src/main/java/uk/ac/ebi/subs/data/component/Contact.java
@@ -7,15 +7,16 @@ import java.util.List;
 
 @Data
 public class Contact {
-    private String orcid;
     private String firstName;
     private String middleInitials;
     private String lastName;
     private String email;
     private List<String> roles = new ArrayList<>();
-    private String address;
-    private String affiliation;
+    private String orcid;
     private String phone;
     private String fax;
-
+    private String affiliation;
+    private String address;
+    private String affiliationURI;
+    private String affiliationRole;
 }
